b3d6d0997482a92cada6c14e61a4f6258193645f
Avi Vaid <avaid1996@gmail.com>
2016-07-29 12:16:51 -0700

    added functionality for snippet saving etc
    Signed-off-by: Avi Vaid <avaid1996@gmail.com>

--- starterbot.py
+++ starterbot.py
@@ -2,24 +2,32 @@ import os
 import time
 from slackclient import SlackClient
 import re
+import requests
+from fuzzywuzzy import fuzz
+
 # starterbot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
 
 # constants
-BOT_ID = "U1U4WHL92"
 AT_BOT = "<@" + BOT_ID + ">:"
 SAVE_COMMAND = "save"
 GET_COMMAND = "get"
 
+# to be replaced by database add row function
 def save(command):
     command = command[len(SAVE_COMMAND):]
+    # db call to save the string here
     return command
 
+# to be used to look up a person
 def person(name):
-    avi = "avi"
-    if avi.lower() == name.lower():
+    print name
+    if "name=" in name:
         return True
+    return False
 
+# to be used to get history
 def get(command):
     command = command[len(SAVE_COMMAND):]
     datereg = re.compile(r'\d{2}\/\d{2}\/\d{4}')
@@ -29,14 +37,57 @@ def get(command):
         querywords = command.split(' ')
         people = filter((lambda word: person(word)), querywords)
         if len(people) > 0:
+            # here is where you should get a specific person's/people's standup
             return date.group() + ' ' +' '.join(people)
+        #g here is where you get the full snippet for that date
         return date.group()
     else:
-        querywords = command.split(' ')
-        people = filter((lambda word: person(word)), querywords)
-        if len(people) > 0:
-            return ' '.join(people)
-        return "error"
+        return "please specify a date"
+
+# used to parse a snippet's text to return personwise standup
+def parsesnippet(sniptxt):
+    lines = sniptxt.split("\n")
+    for line in lines[1:]:
+        if line=="\r":
+            continue
+        personreg = re.compile(r'([A-Z]|[a-z])+:.+')
+        personst = personreg.search(line)
+        if personst is not None:
+            personst = personst.group()
+            splitst = personst.split(":", 1)
+            person = split[0]
+
+    return None
+
+# returns true if it is a standup or a sync
+def isstandup(sniptxt):
+    lines = sniptxt.split("\n")
+    matchst = fuzz.partial_ratio("standup", lines[0].lower()) 
+    matchsy = fuzz.partial_ratio("sync", lines[0].lower()) 
+    if matchst > 70 or matchsy > 70:
+        return True
+    return False
+
+# to be used to save a snippet where we save the id of it if it meets a criterion
+def savesnippet(fileid):
+    url = "https://slack.com/api/files.info?token=%s&file=%s&pretty=1" % (SLACK_TOKEN, fileid)
+    response = requests.request("GET", url)
+    resjson = response.json()
+    if resjson and "file" in resjson and "url_private_download" in resjson["file"]:
+        url = resjson['file']["url_private_download"]
+        if url.endswith(".txt"):
+            auth = "Bearer %s" % SLACK_TOKEN
+            headers = {
+                    'authorization': auth,
+                        'cache-control': "no-cache",
+                            }
+            response = requests.request("GET", url, headers=headers)
+            sniptxt = response.text
+            if isstandup(sniptxt):
+                # put in a database call to save the fileid here
+                print fileid
+                return True
+    return False
 
 def handle_command(command, channel):
     """
@@ -50,6 +101,14 @@ def handle_command(command, channel):
         response = save(command)
     if command.startswith(GET_COMMAND):
         response = get(command)
+    if command.startswith("id:"):
+        # make sure it's a snippet and not another file that we may end up caching
+        command = command[3:]
+        response = savesnippet(command)  
+        if response is False:
+            return
+        else: 
+            response = "I have tracked your file, you can call me if you need it"
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
@@ -63,6 +122,11 @@ def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
+            if output and 'text' in output and "uploaded" in output['text']:
+                if output and 'file' in output and output['file'] and 'id' in output['file']:
+                        id = "id:"+output['file']['id']
+                        return id, \
+                                output['channel']
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
